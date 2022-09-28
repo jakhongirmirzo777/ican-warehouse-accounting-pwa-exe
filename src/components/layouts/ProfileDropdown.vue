a15c42ce3d68b1872871f84002e3510b8ac2e992
nematoff98 <nematoff98@gmail.com>
2022-09-28 16:32:39 +0500

    changes

---
+++ src/components/layouts/ProfileDropdown.vue
@@ -0,0 +1,64 @@
+<template>
+  <VMenu class="profile-menu cursor-pointer">
+    <template #title>
+      <div class="profile-menu__user-block">
+        <VImg static class="profile-menu__user-block__img" src="user.png" />
+        <div class="profile-menu__user-block__info">
+          <h4
+            class="profile-menu__user-block__info__title"
+            :class="{ 'profile-menu__user-block__info__title__light': theme }"
+          >
+            {{ user?.type?.toUpperCase() }}
+          </h4>
+          <p
+            class="profile-menu__user-block__info__sub-title"
+            :class="{
+              'profile-menu__user-block__info__sub-title__light': theme,
+            }"
+          >
+            {{ user?.username }}
+          </p>
+        </div>
+      </div>
+    </template>
+    <template #body="{ on }">
+      <div
+        v-for="(body, i) in options"
+        :key="`body-${i}`"
+        class="block-hover"
+        @click="onMenuClick(on)"
+      >
+        {{ body.title }}
+      </div>
+    </template>
+  </VMenu>
+</template>
+
+<script lang="ts" setup>
+import VMenu from '@/components/ui/VMenu.vue'
+import VImg from '@/components/ui/VImg.vue'
+
+import { ref } from 'vue'
+import { useUserService } from '@/plugins/user-service'
+import { useI18n } from 'vue-i18n'
+const { logoutUser, user } = useUserService()
+const { t } = useI18n()
+
+defineProps({
+  theme: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const options = ref([{ title: t('logout'), value: 'logout' }])
+
+const onMenuClick = (fn: () => unknown) => {
+  fn()
+  logoutUser()
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/styles/layouts/profile-dropdown.scss';
+</style>
--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -7,42 +7,26 @@
       <VIcon size="100%" icon="logo" />
     </div>
     <div class="header__search">
-      <VInput :label="t('search')" hide-details v-model="search">
+      <VInput :label="$t('search')" hide-details v-model="search">
         <template #prepend>
           <VIcon icon="search" size="16" />
         </template>
       </VInput>
     </div>
-    <VSpacer></VSpacer>
+    <VSpacer class="header__spacer"></VSpacer>
     <div class="header__setting-icons">
-      <VIcon class="mr-3" size="100%" icon="setting" />
-      <VIcon size="100%" icon="notification" />
+      <VIcon
+        class="mr-3 header__setting-icons__setting"
+        size="100%"
+        icon="setting"
+      />
+      <VIcon
+        class="header__setting-icons__notifications"
+        size="100%"
+        icon="notification"
+      />
     </div>
-    <VMenu class="header__profile-menu cursor-pointer">
-      <template #title>
-        <div class="header__user-block">
-          <VImg static class="header__user-block__img" src="user.png" />
-          <div class="header__user-block__info">
-            <h4 class="header__user-block__info__title">
-              {{ user?.type?.toUpperCase() }}
-            </h4>
-            <p class="header__user-block__info__sub-title">
-              {{ user?.username }}
-            </p>
-          </div>
-        </div>
-      </template>
-      <template #body="{ on }">
-        <div
-          v-for="(body, i) in options"
-          :key="`body-${i}`"
-          class="block-hover"
-          @click="onMenuClick(on)"
-        >
-          {{ body.title }}
-        </div>
-      </template>
-    </VMenu>
+    <ProfileDropdown class="header__profile-dropdown" />
   </div>
 </template>
 
@@ -50,23 +34,10 @@
 import VIcon from '@/components/ui/VIcon.vue'
 import VInput from '@/components/ui/VInput.vue'
 import VSpacer from '@/components/ui/VSpacer.vue'
-import VImg from '@/components/ui/VImg.vue'
-import VMenu from '@/components/ui/VMenu.vue'
-
 import { ref } from 'vue'
-import { useUserService } from '@/plugins/user-service'
-import { useI18n } from 'vue-i18n'
-const { logoutUser, user } = useUserService()
-const { t } = useI18n()
-
-const options = ref([{ title: t('logout'), value: 'logout' }])
+import ProfileDropdown from '@/components/layouts/ProfileDropdown.vue'
 
 const search = ref('')
-
-const onMenuClick = (fn: () => unknown) => {
-  fn()
-  logoutUser()
-}
 </script>
 
 <style lang="scss" scoped>
