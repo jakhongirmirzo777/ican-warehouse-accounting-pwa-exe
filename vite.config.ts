83c62220941c4541fa5d9d76e35434a5a5cbc984
00013540 <wiut00013540@gmail.com>
2023-01-01 18:20:38 +0500

    Fix vite basePath

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import ElementPlus from 'unplugin-element-plus/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/stock',
   build: {
     target: 'es2020',
   },
--- vue.config.ts
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line no-undef,@typescript-eslint/ban-ts-comment
-// @ts-ignore
-module.exports = {
-  publicPath: '/stock',
-}
