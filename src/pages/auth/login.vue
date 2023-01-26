eaa8749a54ad5c8080122d2984a1e1c6069a371d
00013540 <wiut00013540@gmail.com>
2023-01-26 20:13:15 +0500

    Fixes

--- src/pages/auth/login.vue
+++ src/pages/auth/login.vue
@@ -4,7 +4,7 @@
     :class="{ dark: theme === THEME.DARK }"
     :style="{ backgroundImage: `url(${backgroundImage})` }"
   >
-    <div style="background-color: white">Release 1.0.4</div>
+    <div style="background-color: blue">Release 1.0.7</div>
     <div class="login__box">
       <div class="login__box--top">
         <Form @submit="onSubmit">
