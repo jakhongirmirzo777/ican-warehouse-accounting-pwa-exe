3657c5e3b90ea95541ceb31d5d3702fd600d741f
00013540 <wiut00013540@gmail.com>
2023-01-27 15:24:25 +0500

    Changes done

--- main.js
+++ main.js
@@ -1,41 +1,71 @@
-const { app, BrowserWindow, autoUpdater, dialog } = require('electron')
 const path = require('path')
-
-const server = 'https://ican-warehouse-accounting-pwa-exe-server.vercel.app/'
-const url = `${server}/update/${process.platform}/${app.getVersion()}`
-
-autoUpdater.setFeedURL({ url })
+const { app, dialog, BrowserWindow } = require('electron')
+const { autoUpdater } = require('electron-updater')
+const isDev = require('electron-is-dev')
 
 const createWindow = () => {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      enableRemoteModule: true,
     },
   })
 
-  mainWindow.loadFile('dist/index.html')
-  mainWindow.webContents.openDevTools()
+  win.loadURL(
+    isDev
+      ? 'http://localhost:4000'
+      : `file://${path.join(__dirname, '../dist/index.html')}`
+  )
+
+  // Open the DevTools.
+  if (isDev) {
+    win.webContents.openDevTools({ mode: 'detach' })
+    require('react-devtools-electron')
+  }
+
+  if (!isDev) {
+    autoUpdater.checkForUpdates()
+  }
 }
 
-app.whenReady().then(() => {
-  createWindow()
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow)
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
-
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
 
-setInterval(() => {
-  autoUpdater.checkForUpdates()
-}, 60000)
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
 
-autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+autoUpdater.on('update-available', (_event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Ok'],
+    title: 'Application Update',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail: 'A new version is being downloaded.',
+  }
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  dialog.showMessageBox(dialogOpts, (_response) => {})
+})
+
+autoUpdater.on('update-downloaded', (_event, releaseNotes, releaseName) => {
   const dialogOpts = {
     type: 'info',
     buttons: ['Restart', 'Later'],
@@ -44,13 +74,7 @@ autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
     detail:
       'A new version has been downloaded. Restart the application to apply the updates.',
   }
-
   dialog.showMessageBox(dialogOpts).then((returnValue) => {
     if (returnValue.response === 0) autoUpdater.quitAndInstall()
   })
-})
-
-autoUpdater.on('error', (message) => {
-  console.error('There was a problem updating the application')
-  console.error(message)
 })
--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: path.resolve(__dirname, './dist'),
+  base: path.resolve(__dirname, './dist/'),
   build: {
     target: 'es2020',
   },
