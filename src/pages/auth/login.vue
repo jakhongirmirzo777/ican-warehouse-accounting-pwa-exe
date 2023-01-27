847cd0809645ec3060a66c168ef3ec7dcb2b4d69
00013540 <wiut00013540@gmail.com>
2023-01-27 17:34:18 +0500

    Change color

--- src/pages/auth/login.vue
+++ src/pages/auth/login.vue
@@ -4,7 +4,7 @@
     :class="{ dark: theme === THEME.DARK }"
     :style="{ backgroundImage: `url(${backgroundImage})` }"
   >
-    <div style="background-color: yellow">Release 1.0.10</div>
+    <div style="background-color: red">Release 1.0.15</div>
     <div class="login__box">
       <div class="login__box--top">
         <Form @submit="onSubmit">
